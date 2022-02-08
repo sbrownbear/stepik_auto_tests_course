fbb64941ec9842fd70d9afd64e156b588757e57b
Sergey Konoplev <sergey.gsarh@gmail.com>
2022-02-08 20:42:31 +0300

    Initial commit

---
+++ 2_1_7.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from math import log, sin
+
+link = "http://suninjuly.github.io/get_attribute.html"
+
+browser = webdriver.Chrome()
+
+# Открыть страницу
+browser.get(link)
+
+# Найти на ней элемент-картинку/ Взять у этого элемента значение атрибута valuex
+valuex = browser.find_element_by_css_selector('[id = "treasure"]').get_attribute('valuex')
+
+# Посчитать математическую функцию от x, Ввести ответ в текстовое поле.
+browser.find_element_by_id('answer').send_keys(str(log(abs(12 * sin(int(valuex))))))
+
+# Отметить checkbox "Подтверждаю, что являюсь роботом". Выбрать radiobutton "Роботы рулят!". Нажать на кнопку Отправить.
+for selector in ['#robotCheckbox', '#robotsRule', '.btn.btn-default']:
+  browser.find_element_by_css_selector(selector).click()
